e5afcd1a1fc9fb4199de7a9be3c36c6ed034072a
Bo Yuan <boyuxa@syntronic.com>
2023-12-02 14:15:39 -0500

    removed bootstrp js added code to handle history stack added code to change page title

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -2,35 +2,100 @@
 //Note: This can be changed to whatever JavaScript formatting you would like
 let tabBtns = [];
 let tabPanels = {};
+let documenTitles = ["Home", "Service", "Schedule a Service"];
 
 tabBtns = Array.from(document.querySelectorAll("#v-pills-tab a"));
 
-
-for (let i =0 ; i < tabBtns.length; i++) {
-	tabBtns[i].addEventListener("click", selectTab, false);
-	tabBtns[i].addEventListener("keyup", focusTab, false);
+for (let i = 0; i < tabBtns.length; i++) {
+  tabBtns[i].addEventListener("click", selectTab, false);
 }
 
-// If left (37) or right (39) arrow keys are pressed, change the focus on the tab buttons, but do not activate.
-function focusTab (e) {
-	if (e.keyCode == 39) {
-		tabBtns[(tabBtns.indexOf(e.target) + 1) % 3].focus();
-	} else if (e.keyCode == 37) {
-		tabBtns[((tabBtns.indexOf(e.target) - 1) < 0 ? 2 : tabBtns.indexOf(e.target) - 1)].focus();
-	}
-} // End of focusTab
+let x = document.querySelectorAll(".tab-pane");
+for (let i = 0; i < x.length; i++) {
+  tabPanels[x[i].id] = x[i];
+}
 
+
+function applyTab(id) {
+  for (var i = 0; i < tabBtns.length; i++) {
+    if (tabBtns[i].id == id) {
+      let tabPanelID = tabBtns[i].id.replace("-tab", "-panel");
+      tabPanels[tabPanelID].classList.add("show");
+      tabPanels[tabPanelID].classList.add("active");
+      tabBtns[i].setAttribute("aria-selected", "true");
+      tabBtns[i].parentNode.classList.add("selectedTab");
+      tabBtns[i].classList.add("active");
+      document.title = documenTitles[i];
+    } else {
+      let tabPanelID = tabBtns[i].id.replace("-tab", "-panel");
+      tabPanels[tabPanelID].classList.remove("show");
+      tabPanels[tabPanelID].classList.remove("active");
+      tabBtns[i].setAttribute("aria-selected", "false");
+      tabBtns[i].parentNode.classList.remove("selectedTab");
+      tabBtns[i].classList.remove("active");
+    }
+  }
+}
 
 function selectTab(e) {
+  applyTab(e.target.id);
+}
 
-	for (var i = 0; i < tabBtns.length; i++) {
-		if (tabBtns[i].id == e.target.id) {
-			tabBtns[i].setAttribute("aria-selected", "true");
-			tabBtns[i].parentNode.classList.add("selectedTab");			
-		} else {
-			tabBtns[i].setAttribute("aria-selected", "false");			
-			tabBtns[i].parentNode.classList.remove("selectedTab");
-            tabBtns[i].classList.remove("active");
-		}
-	}
-} // End of selectTab
+function historyTabs(selector) {
+  var elements = document.querySelectorAll(selector);
+  window.addEventListener("popstate", function (event) {
+    if (event.state) {
+      var targetTab = Array.from(elements).find(function (element) {
+        return element.getAttribute("href") === event.state.url;
+      });
+
+      if (targetTab) {
+        // Manually implement tab showing logic (e.g., add 'active' class).
+        showTab(targetTab);
+      }
+    }
+  });
+  Array.from(elements).forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault();
+      showTab(element);
+      var stateObject = { url: element.getAttribute("href") };
+      if (!window.location.hash || stateObject.url !== window.location.hash) {
+        var newUrl = window.location.pathname + stateObject.url;
+        window.history.pushState(stateObject, document.title, newUrl);
+      } else {
+        window.history.replaceState(
+          stateObject,
+          document.title,
+          window.location.href
+        );
+      }
+    });
+
+    if (!window.location.hash && element.classList.contains("active")) {
+      // Shows the first element if there are no query parameters.
+      var stateObject = { url: element.getAttribute("href") };
+      if (!window.location.hash || stateObject.url !== window.location.hash) {
+        var newUrl = window.location.pathname + stateObject.url;
+        window.history.pushState(stateObject, document.title, newUrl);
+      } else {
+        window.history.replaceState(
+          stateObject,
+          document.title,
+          window.location.href
+        );
+      }
+      showTab(element);
+    } else if (element.getAttribute("href") === window.location.hash) {
+      // Manually implement tab showing logic.
+      showTab(element);
+    }
+  });
+
+  function showTab(tabElement) {
+    applyTab(tabElement.id);
+  }
+}
+
+// Usage
+historyTabs('a[data-toggle="pill"]');
