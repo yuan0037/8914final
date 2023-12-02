9271638e720417083ec07aaf7fe70b6697504002
Bo Yuan <boyuxa@syntronic.com>
2023-12-02 15:44:15 -0500

    added trap focus on modal

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -1,6 +1,51 @@
 //Template Function that can be used to run JavaScript on the page
 //Note: This can be changed to whatever JavaScript formatting you would like
 
+
+const trapFocus = (element, prevFocusableElement = document.activeElement) => {
+    const focusableEls = Array.from(
+      element.querySelectorAll(
+        'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
+      )
+    );
+    const firstFocusableEl = focusableEls[0];
+    const lastFocusableEl = focusableEls[focusableEls.length - 1];
+    let currentFocus = null;
+
+    firstFocusableEl.focus();
+    currentFocus = firstFocusableEl;
+
+    const handleFocus = e => {
+      e.preventDefault();
+      // if the focused element "lives" in your modal container then just focus it
+      if (focusableEls.includes(e.target)) {
+        currentFocus = e.target;
+      } else {
+        // you're out of the container
+        // if previously the focused element was the first element then focus the last 
+        // element - means you were using the shift key
+        if (currentFocus === firstFocusableEl) {
+          lastFocusableEl.focus();
+        } else {
+          // you previously were focused on the last element so just focus the first one
+          firstFocusableEl.focus();
+        }
+        // update the current focus var
+        currentFocus = document.activeElement;
+      }
+    };
+
+    document.addEventListener("focus", handleFocus, true);
+
+    return {
+      onClose: () => {
+        document.removeEventListener("focus", handleFocus, true);
+        prevFocusableElement.focus();
+      }
+    };
+  };
+
+
 var toggleButton = document.getElementById('communityBtn');
        
 // Add a click event listener to the button
@@ -23,9 +68,11 @@ function toggleModal(isOpen) {
 	if (isOpen) {
 		modal.classList.add("show");
 		modal.style.display = "block";
+		trapped = trapFocus(modal);
 	} else {	
 		modal.classList.remove("show");
 		modal.style.display = "none";
+		trapped.onClose();
 	}
 }
 
